394ffcd8292aac968aead89633c8ac3f8f3862ef
Evelyn Dutra <evelyn.dutra@estudante.ifms.edu.br>
2026-05-09 02:26:06 -0300

    criei o pacote bean e fiz cliente e usuarios

---
+++ src/bean/ed_cliente.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author DELL
+ */
+public class ed_cliente {
+    
+     private int ed_idCliente;
+    private String ed_Nome;
+    private String ed_Cpf;
+    private String ed_Rg;
+    private String ed_Sexo;
+    private String ed_Email;
+    private String ed_Telefone;
+    private String ed_Celular;
+    private String ed_Cep;
+    private String ed_Logradouro;
+    private String ed_Numero;
+    private String ed_Bairro;
+    private String ed_Cidade;
+    private Date ed_DataCadastro;
+    private String ed_Ativo;
+
+    public int getEd_idCliente() {
+        return ed_idCliente;
+    }
+
+    public void setEd_idCliente(int ed_idCliente) {
+        this.ed_idCliente = ed_idCliente;
+    }
+
+    public String getEd_Nome() {
+        return ed_Nome;
+    }
+
+    public void setEd_Nome(String ed_Nome) {
+        this.ed_Nome = ed_Nome;
+    }
+
+    public String getEd_Cpf() {
+        return ed_Cpf;
+    }
+
+    public void setEd_Cpf(String ed_Cpf) {
+        this.ed_Cpf = ed_Cpf;
+    }
+
+    public String getEd_Rg() {
+        return ed_Rg;
+    }
+
+    public void setEd_Rg(String ed_Rg) {
+        this.ed_Rg = ed_Rg;
+    }
+
+    public String getEd_Sexo() {
+        return ed_Sexo;
+    }
+
+    public void setEd_Sexo(String ed_Sexo) {
+        this.ed_Sexo = ed_Sexo;
+    }
+
+    public String getEd_Email() {
+        return ed_Email;
+    }
+
+    public void setEd_Email(String ed_Email) {
+        this.ed_Email = ed_Email;
+    }
+
+    public String getEd_Telefone() {
+        return ed_Telefone;
+    }
+
+    public void setEd_Telefone(String ed_Telefone) {
+        this.ed_Telefone = ed_Telefone;
+    }
+
+    public String getEd_Celular() {
+        return ed_Celular;
+    }
+
+    public void setEd_Celular(String ed_Celular) {
+        this.ed_Celular = ed_Celular;
+    }
+
+    public String getEd_Cep() {
+        return ed_Cep;
+    }
+
+    public void setEd_Cep(String ed_Cep) {
+        this.ed_Cep = ed_Cep;
+    }
+
+    public String getEd_Logradouro() {
+        return ed_Logradouro;
+    }
+
+    public void setEd_Logradouro(String ed_Logradouro) {
+        this.ed_Logradouro = ed_Logradouro;
+    }
+
+    public String getEd_Numero() {
+        return ed_Numero;
+    }
+
+    public void setEd_Numero(String ed_Numero) {
+        this.ed_Numero = ed_Numero;
+    }
+
+    public String getEd_Bairro() {
+        return ed_Bairro;
+    }
+
+    public void setEd_Bairro(String ed_Bairro) {
+        this.ed_Bairro = ed_Bairro;
+    }
+
+    public String getEd_Cidade() {
+        return ed_Cidade;
+    }
+
+    public void setEd_Cidade(String ed_Cidade) {
+        this.ed_Cidade = ed_Cidade;
+    }
+
+    public Date getEd_DataCadastro() {
+        return ed_DataCadastro;
+    }
+
+    public void setEd_DataCadastro(Date ed_DataCadastro) {
+        this.ed_DataCadastro = ed_DataCadastro;
+    }
+
+    public String getEd_Ativo() {
+        return ed_Ativo;
+    }
+
+    public void setEd_Ativo(String ed_Ativo) {
+        this.ed_Ativo = ed_Ativo;
+    }
+}
---
+++ src/bean/ed_usuarios.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author u09722316176
+ */
+public class ed_usuarios {
+    private int ed_idUsuarios;
+    private String ed_Nome;
+    private String ed_Apelido;
+    private String ed_Cpf;
+    private Date ed_DataDeNascimento;
+    private int edNivel;
+    private String ed_Senha;
+    private String ed_Ativo;
+
+    /**
+     * @return the ed_idUsuarios
+     */
+    public int getEd_idUsuarios() {
+        return ed_idUsuarios;
+    }
+
+    /**
+     * @param ed_idUsuarios the ed_idUsuarios to set
+     */
+    public void setEd_idUsuarios(int ed_idUsuarios) {
+        this.ed_idUsuarios = ed_idUsuarios;
+    }
+
+    /**
+     * @return the ed_Nome
+     */
+    public String getEd_Nome() {
+        return ed_Nome;
+    }
+
+    /**
+     * @param ed_Nome the ed_Nome to set
+     */
+    public void setEd_Nome(String ed_Nome) {
+        this.ed_Nome = ed_Nome;
+    }
+
+    /**
+     * @return the ed_Apelido
+     */
+    public String getEd_Apelido() {
+        return ed_Apelido;
+    }
+
+    /**
+     * @param ed_Apelido the ed_Apelido to set
+     */
+    public void setEd_Apelido(String ed_Apelido) {
+        this.ed_Apelido = ed_Apelido;
+    }
+
+    /**
+     * @return the ed_Cpf
+     */
+    public String getEd_Cpf() {
+        return ed_Cpf;
+    }
+
+    /**
+     * @param ed_Cpf the ed_Cpf to set
+     */
+    public void setEd_Cpf(String ed_Cpf) {
+        this.ed_Cpf = ed_Cpf;
+    }
+
+    /**
+     * @return the ed_DataDeNascimento
+     */
+    public Date getEd_DataDeNascimento() {
+        return ed_DataDeNascimento;
+    }
+
+    /**
+     * @param ed_DataDeNascimento the ed_DataDeNascimento to set
+     */
+    public void setEd_DataDeNascimento(Date ed_DataDeNascimento) {
+        this.ed_DataDeNascimento = ed_DataDeNascimento;
+    }
+
+    /**
+     * @return the edNivel
+     */
+    public int getEdNivel() {
+        return edNivel;
+    }
+
+    /**
+     * @param edNivel the edNivel to set
+     */
+    public void setEdNivel(int edNivel) {
+        this.edNivel = edNivel;
+    }
+
+    /**
+     * @return the ed_Senha
+     */
+    public String getEd_Senha() {
+        return ed_Senha;
+    }
+
+    /**
+     * @param ed_Senha the ed_Senha to set
+     */
+    public void setEd_Senha(String ed_Senha) {
+        this.ed_Senha = ed_Senha;
+    }
+
+    /**
+     * @return the ed_Ativo
+     */
+    public String getEd_Ativo() {
+        return ed_Ativo;
+    }
+
+    /**
+     * @param ed_Ativo the ed_Ativo to set
+     */
+    public void setEd_Ativo(String ed_Ativo) {
+        this.ed_Ativo = ed_Ativo;
+    }
+}
